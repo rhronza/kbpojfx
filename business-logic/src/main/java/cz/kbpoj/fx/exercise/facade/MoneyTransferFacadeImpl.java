7aaf61b279ed0c0e73a27725542b1b1db20ef496
Roman Hronza <roman.hronza@gmail.com>
2023-01-20 20:37:16 +0100

    update facade and service

--- business-logic/src/main/java/cz/kbpoj/fx/exercise/facade/MoneyTransferFacadeImpl.java
+++ business-logic/src/main/java/cz/kbpoj/fx/exercise/facade/MoneyTransferFacadeImpl.java
@@ -3,6 +3,7 @@ package cz.kbpoj.fx.exercise.facade;
 import cz.kbpoj.fx.exercise.domain.TargetAmount;
 import cz.kbpoj.fx.exercise.enumeration.Currency;
 import cz.kbpoj.fx.exercise.exception.KbpojCurrenciesMustBeDifferentException;
+import cz.kbpoj.fx.exercise.exception.KbpojDayIsOlderThenOneMonthException;
 import cz.kbpoj.fx.exercise.service.MoneyTransferService;
 import org.springframework.stereotype.Service;
 
@@ -19,13 +20,16 @@ public class MoneyTransferFacadeImpl implements MoneyTransferFacade {
 
     @Override
     public TargetAmount make(LocalDate dayCourse, Currency sourceCurrency, Float amount, Currency targerCurrency) {
+        if (LocalDate.now().minusMonths(1).isAfter(dayCourse)) {
+           throw new KbpojDayIsOlderThenOneMonthException("Day is older than 1 month.", dayCourse);
+        }
         TargetAmount targetAmount;
         if (sourceCurrency == targerCurrency) {
             throw new KbpojCurrenciesMustBeDifferentException("Source and target currencies must be different.", sourceCurrency);
         } else if (sourceCurrency == Currency.CZK) {
             targetAmount = moneyTransferService.transferFromCzkToForeignMoney(dayCourse, amount, targerCurrency);
         } else if (targerCurrency == Currency.CZK) {
-            targetAmount = moneyTransferService.transferFromForeignMoneyToCzk(dayCourse, amount, targerCurrency);
+            targetAmount = moneyTransferService.transferFromForeignMoneyToCzk(dayCourse, amount, sourceCurrency);
         } else
             targetAmount = moneyTransferService.transferFromForeignMoneyToForeignMoney(dayCourse, sourceCurrency, amount, targerCurrency);
         return targetAmount;
--- business-logic/src/main/java/cz/kbpoj/fx/exercise/service/MoneyTransferServiceImpl.java
+++ business-logic/src/main/java/cz/kbpoj/fx/exercise/service/MoneyTransferServiceImpl.java
@@ -1,25 +1,69 @@
 package cz.kbpoj.fx.exercise.service;
 
 import cz.kbpoj.fx.exercise.domain.TargetAmount;
+import cz.kbpoj.fx.exercise.entity.CurrencyCourseEntity;
+import cz.kbpoj.fx.exercise.entity.DailyStatementEntity;
 import cz.kbpoj.fx.exercise.enumeration.Currency;
+import cz.kbpoj.fx.exercise.exception.KbpojNotFoundException;
+import cz.kbpoj.fx.exercise.exception.KeyValue;
+import cz.kbpoj.fx.exercise.repository.DailyStatementEntityRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class MoneyTransferServiceImpl implements MoneyTransferService {
-    @Override
-    public TargetAmount transferFromCzkToForeignMoney(LocalDate dayCourse, Float amount, Currency targetMoney) {
-        return new TargetAmount(100F, Currency.BRL);
+
+    private final DailyStatementEntityRepository dailyStatementEntityRepository;
+
+    public MoneyTransferServiceImpl(DailyStatementEntityRepository dailyStatementEntityRepository) {
+        this.dailyStatementEntityRepository = dailyStatementEntityRepository;
     }
 
     @Override
-    public TargetAmount transferFromForeignMoneyToCzk(LocalDate dayCourse, Float amount, Currency targetMoney) {
-        return new TargetAmount(200F, Currency.BRL);
+    public TargetAmount transferFromCzkToForeignMoney(LocalDate dayCourse, Float amount, Currency targetCurrency) {
+        CurrencyCourseEntity currencyCourseEntitiesSearched = geSearchedCourse(dayCourse, targetCurrency);
+
+        return new TargetAmount(
+                (amount / currencyCourseEntitiesSearched.getCourse()) * currencyCourseEntitiesSearched.getAmount(),
+                targetCurrency);
+
+    }
+
+    @Override
+    public TargetAmount transferFromForeignMoneyToCzk(LocalDate dayCourse, Float amount, Currency targetCurrency) {
+        CurrencyCourseEntity currencyCourseEntitiesSearched = geSearchedCourse(dayCourse, targetCurrency);
+        return new TargetAmount(
+                (amount * currencyCourseEntitiesSearched.getCourse()) / currencyCourseEntitiesSearched.getAmount(),
+                targetCurrency);
     }
 
     @Override
     public TargetAmount transferFromForeignMoneyToForeignMoney(LocalDate dayCourse, Currency sourceCurrency, Float amount, Currency targetCurrency) {
-        return new TargetAmount(300F, Currency.BRL);
+        TargetAmount targetAmountInCzk = this.transferFromForeignMoneyToCzk(dayCourse, amount, sourceCurrency);
+        TargetAmount targetAmount = this.transferFromCzkToForeignMoney(dayCourse, targetAmountInCzk.targetAmount(), targetCurrency);
+        return new TargetAmount(targetAmount.targetAmount(), targetCurrency);
     }
+
+    private CurrencyCourseEntity geSearchedCourse(LocalDate dayCourse, Currency targetCurrency) {
+        List<DailyStatementEntity> byCourseDay = dailyStatementEntityRepository
+                .findByCourseDay(dayCourse);
+        if (byCourseDay == null || byCourseDay.isEmpty()) {
+            throw new KbpojNotFoundException("Courses not found.", List.of(new KeyValue("day", dayCourse.toString())));
+        }
+        Set<CurrencyCourseEntity> currencyCourseEntities = byCourseDay.get(0).getCurrencyCourseEntities();
+        if (currencyCourseEntities == null || currencyCourseEntities.isEmpty()) {
+            throw new KbpojNotFoundException("Courses not found .", List.of(new KeyValue("day", dayCourse.toString()),
+                    new KeyValue("list is empty", "")));
+        }
+        List<CurrencyCourseEntity> currencyCourseEntitiesSearched = currencyCourseEntities.stream().filter(e -> targetCurrency.name().equals(e.getCurrency())).toList();
+        if (currencyCourseEntitiesSearched.isEmpty()) {
+            throw new KbpojNotFoundException("Course not found .", List.of(new KeyValue("day", dayCourse.toString()),
+                    new KeyValue("target currency", targetCurrency.name())));
+        }
+        return currencyCourseEntitiesSearched.get(0);
+    }
+
 }
