d80a97293e27790f86627c286f90e6ccfa7a4187
Roman Hronza <roman.hronza@gmail.com>
2023-01-20 10:46:45 +0100

    add Currency enum

---
+++ business-logic/src/main/java/cz/kbpoj/fx/exercise/enumeration/Currency.java
@@ -0,0 +1,38 @@
+package cz.kbpoj.fx.exercise.enumeration;
+
+public enum Currency {
+        CZK,
+        USD,
+        EUR,
+        AUD,
+        BRL,
+        BGN,
+        CNY,
+        DKK,
+        PHP,
+        HKD,
+        INR,
+        ODR,
+        ISK,
+        ILS,
+        JPY,
+        ZAR,
+        CAD,
+        KRW,
+        HUF,
+        MYR,
+        MXN,
+        XDR,
+        NOK,
+        NZD,
+        PLN,
+        RON,
+        SGD,
+        SEK,
+        CHR,
+        THB,
+        TRY,
+        GBP
+
+        
+}
