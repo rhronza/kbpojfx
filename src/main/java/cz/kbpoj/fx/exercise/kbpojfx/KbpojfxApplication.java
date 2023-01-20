440f48298d9f02db1112bafdefc5fa1adb324b7a
Roman Hronza <roman.hronza@gmail.com>
2023-01-20 08:52:49 +0100

    init commit

---
+++ src/main/java/cz/kbpoj/fx/exercise/kbpojfx/KbpojfxApplication.java
@@ -0,0 +1,13 @@
+package cz.kbpoj.fx.exercise.kbpojfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbpojfxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbpojfxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/kbpoj/fx/exercise/kbpojfx/KbpojfxApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.kbpoj.fx.exercise.kbpojfx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbpojfxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
